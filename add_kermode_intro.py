5acbcaa6200a78bfd9410295c9be3d4edd72272c
Thomas Hudson <07thomas.hudson@gmail.com>
2022-06-09 14:01:31 +0100

    Update add_kermode_intro.py

--- add_kermode_intro.py
+++ add_kermode_intro.py
@@ -1,6 +1,7 @@
 '''Downloads BFI 'Mark Kermode Reviews' video to related films'''
 #
 # Requires the movies_dir path to be set to your movies library
+# This will miss some movies, but shouldn't match incorrectly.
 #
 import os
 import glob
@@ -37,13 +38,15 @@ def find_movie(title, year=None):
         except:
             pass
 
-# Download playlist titles
-if not os.path.exists("bfi"):
+# Download playlist titles - delete this os.path.exists check to update playlist
+if not os.path.exists("/tmp/bfi"):
     os.mkdir("/tmp/bfi")
     os.system("cd /tmp/bfi && youtube-dl --ignore-errors --write-info-json --skip-download 'https://www.youtube.com/playlist?list=PLXvkgGofjDzhx-h7eexfVbH3WslWrBXE9'")
 
 for fn in glob.glob("/tmp/bfi/*.json"):
     data = json.load(open(fn))
+    
+    # Extract movie name and (hopefully) year from youtube video title
     movie_title = data["title"]
     movie_title = movie_title.split("|")[0]
     movie_title = movie_title.replace("BFI Player", "")
@@ -55,19 +58,31 @@ for fn in glob.glob("/tmp/bfi/*.json"):
     movie_title = re.sub(r'\(.+\)', '', movie_title).strip()
     movie = None
     search_str = movie_title
+    
+    # Sanity check in case year is a string
+    if movie_year is not None and not movie_year.isnumeric():
+        movie_year = None
+        
+    # Try and find a matching movie in the database
     while movie is None:
         movie = find_movie(search_str, movie_year)
         if " " not in search_str:
             break
         search_str = search_str.split(" ", 1)[-1]
+        
     if movie is not None:
         if movie["Name"].lower() not in movie_title.lower():
             movie = None
 
+    # Download Kermode intro if it doesn't already exist
     print(movie_title, movie_year)
     if movie is not None:
         print("\tDownloading", movie["Name"], movie["Id"])
-        movie_filepath = movies_dir + "/" + movie["Name"] + " (" + str(movie["ProductionYear"]) + ")/extras"
+        movie_filepath = movies_dir + "/" + movie["Name"].replace("'","\'") + " (" + str(movie["ProductionYear"]) + ")/extras"
         if not os.path.exists(movie_filepath):
-            # os.mkdir(movie_filepath)
-            print("yt -i https://youtube.com/watch?v="+data["id"] + " --output '" + movie_filepath + "/Mark Kermode Introduces'")
+            os.mkdir(movie_filepath)
+        if len(glob.glob(movie_filepath+"/Mark Kermode*")) == 0:
+            print("\tDownloading", movie["Name"], "intro", movie["Id"])
+            os.system("youtube-dl -i https://youtube.com/watch?v="+data["id"] + " --output '" + movie_filepath + "/Mark Kermode Introduces'")
+        else:
+            print("\tIntro already Exists")
