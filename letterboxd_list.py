394e39bbdb0d0b635ca0195a03e23c7b1aba0c76
Mike Zrimsek <mikezrimsek@gmail.com>
2024-02-28 10:55:57 -0500

    Feature/multi page letterboxd (#7)
    * minor refactor
    
    * update script to query letterboxd pages

--- letterboxd_list.py
+++ letterboxd_list.py
@@ -5,6 +5,26 @@ import html
 
 from utils import load_app_config, request_repeat_get, request_repeat_post, find_collection_with_name_or_create, get_all_collections
 
+def get_collection_data_from_letterboxd_list(list_id: str):
+    '''Get all movies from a Letterboxd list'''
+    page_number = 1
+    movies = []
+    list_name = None
+    
+    while True:
+        request_base = f'https://letterboxd.com/{list_id}/detail/by/release-earliest/page/{page_number}/'
+        res = requests.get(request_base)
+        
+        if list_name is None:
+            list_name = html.unescape(res.text.split('<h1 class="title-1 prettify">')[1].split("</h1>")[0]).strip()
+        page_movies = res.text.split('film-detail-content">')[1:]
+        
+        if len(page_movies) == 0:
+            return {"list_name": list_name, "movies": movies}
+        
+        movies += page_movies
+        page_number += 1
+
 def update_letterboxd_list_collections(app_config: dict):
     '''Make collections from Letterboxd lists'''
     server_url = app_config["server_url"]
@@ -26,13 +46,17 @@ def update_letterboxd_list_collections(app_config: dict):
         # Parse letterboxd page
         print()
         print()
-        res = requests.get(f'https://letterboxd.com/{list_id}/detail/by/release-earliest')
-        list_name = html.unescape(res.text.split('<h1 class="title-1 prettify">')[1].split("</h1>")[0]).strip()
+        
+        collection_data = get_collection_data_from_letterboxd_list(list_id)
+        list_name = collection_data["list_name"]
+        movies = collection_data["movies"]
+        
         collection_id = find_collection_with_name_or_create(server_url, list_name, collections, headers=headers)
+        
         print("************************************************")
         print()
 
-        for movie in res.text.split('film-detail-content">')[1:]:
+        for movie in movies:
             movie_title = html.unescape(movie.split('<a href="')[1].split('>')[1].split("<")[0])
             movie_year = movie.split('metadata">')[1].split('<a href="')[1].split('>')[1].split("<")[0]
             params2 = params.copy()
