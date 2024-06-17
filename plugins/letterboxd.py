a3e36e6c8fbc6a101b05dc57f15271386d8fefc8
Thomas Hudson <13795113+ghomasHudson@users.noreply.github.com>
2024-06-17 15:05:25 +0100

    Made letterboxd more robust #38

--- plugins/letterboxd.py
+++ plugins/letterboxd.py
@@ -2,6 +2,8 @@ import json
 from utils.base_plugin import ListScraper
 import bs4
 import requests
+import time
+from loguru import logger
 
 class Letterboxd(ListScraper):
 
@@ -14,7 +16,9 @@ class Letterboxd(ListScraper):
         movies = []
 
         while True:
+            print("Page number: ", page_number)
             r = requests.get(f"https://letterboxd.com/{list_id}/detail/by/release-earliest/page/{page_number}/", headers={'User-Agent': 'Mozilla/5.0'})
+
             soup = bs4.BeautifulSoup(r.text, 'html.parser')
 
             if list_name is None:
@@ -26,15 +30,18 @@ class Letterboxd(ListScraper):
 
             for movie_soup in soup.find_all('div', {'class': 'film-detail-content'}):
                 movie_name = movie_soup.find('h2', {'class': 'headline-2 prettify'}).find('a').text
-                movie_year = movie_soup.find('small', {'class': 'metadata'}).text
+                movie_year = movie_soup.find('small', {'class': 'metadata'})
+                if movie_year is not None:
+                    movie_year = movie_year.text
                 movie = {"title": movie_name, "release_year": movie_year, "media_type": "movie"}
 
                 # Find the imdb id
                 if config.get("imdb_id_filter", False):
                     r = requests.get(f"https://letterboxd.com{movie_soup.find('a')['href']}", headers={'User-Agent': 'Mozilla/5.0'})
                     movie_soup = bs4.BeautifulSoup(r.text, 'html.parser')
-                    imdb_id = movie_soup.find("a", {"data-track-action":"IMDb"})["href"].split("/title/")[1].split("/")[0]
-                    movie["imdb_id"] = imdb_id
+                    imdb_id = movie_soup.find("a", {"data-track-action":"IMDb"})
+                    if imdb_id is not None:
+                        movie["imdb_id"] = imdb_id["href"].split("/title/")[1].split("/")[0]
 
                 movies.append(movie)
 
