69f6fa03f31f14a76e6acd50fa2f3400929f5431
samueljim <sam@bne.sh>
2024-06-25 09:52:44 +1000

    Use details view if it is not a watchlist to speed up retrieval because we can already get the year data

--- plugins/letterboxd.py
+++ plugins/letterboxd.py
@@ -16,11 +16,15 @@ class Letterboxd(ListScraper):
 
         while True:
             print("Page number: ", page_number)
-            if list_id.endswith("/watchlist"):
+            watchlist = list_id.endswith("/watchlist")
+
+            if watchlist:
+                r = requests.get(f"https://letterboxd.com/{list_id}/by/release-earliest/page/{page_number}/", headers={'User-Agent': 'Mozilla/5.0'})
+
                 list_name = list_id.split("/")[0] + " Watchlist"
                 description = "Watchlist for " + list_id.split("/")[0]
-                
-            r = requests.get(f"https://letterboxd.com/{list_id}/by/release-earliest/page/{page_number}/", headers={'User-Agent': 'Mozilla/5.0'})
+            else:
+                r = requests.get(f"https://letterboxd.com/{list_id}/detail/by/release-earliest/page/{page_number}/", headers={'User-Agent': 'Mozilla/5.0'})
 
             soup = bs4.BeautifulSoup(r.text, 'html.parser')
 
@@ -31,25 +35,42 @@ class Letterboxd(ListScraper):
                 description = soup.find('div', {'class': 'body-text'}).find_all('p')
                 description = "\n".join([p.text for p in description])
 
-            for movie_soup in soup.find_all('li', {'class': 'poster-container'}):
-                movie_name = movie_soup.find('img').attrs['alt']
-                movie = {"title": movie_name, "media_type": "movie"}
+            if watchlist:
+                page = soup.find_all('li', {'class': 'poster-container'})
+            else: 
+                page = soup.find_all('div', {'class': 'film-detail-content'})
 
-                # Find the imdb id and release year
-                r = requests.get(f"https://letterboxd.com{movie_soup.find('div', {'class': 'film-poster'})['data-target-link']}", headers={'User-Agent': 'Mozilla/5.0'})
-                movie_soup = bs4.BeautifulSoup(r.text, 'html.parser')
+            for movie_soup in page:
+                if watchlist:
+                    movie = {"title": movie_soup.find('img').attrs['alt'], "media_type": "movie"}
+                    link = movie_soup.find('div', {'class': 'film-poster'})['data-target-link']
+                else: 
+                    movie = {"title": movie_soup.find('h2', {'class': 'headline-2 prettify'}).find('a').text, "media_type": "movie"}
+                    movie_year = movie_soup.find('small', {'class': 'metadata'})
+                    if movie_year is not None:
+                        movie["release_year"] = movie_year.text
+                        
+                    link = movie_soup.find('a')['href']
+                
+                
+                if config.get("imdb_id_filter", False) or 'release_year' not in movie:
+                    logger.info(f"Getting release year and imdb details for: {movie['title']}")
 
-                imdb_id = movie_soup.find("a", {"data-track-action":"IMDb"})
-                movie_year = movie_soup.find("div", {"class": "releaseyear"})
+                    # Find the imdb id and release year
+                    r = requests.get(f"https://letterboxd.com{link}", headers={'User-Agent': 'Mozilla/5.0'})
+                    movie_soup = bs4.BeautifulSoup(r.text, 'html.parser')
 
-                if imdb_id is not None:
-                    movie["imdb_id"] = imdb_id["href"].split("/title/")[1].split("/")[0]
+                    imdb_id = movie_soup.find("a", {"data-track-action":"IMDb"})
+                    movie_year = movie_soup.find("div", {"class": "releaseyear"})
 
-                if movie_year is not None:
-                    movie["release_year"] = movie_year.text
-                    logger.info(f"Found movie: {movie['title']} ({movie.get('release_year', 'Unknown')}) ({movie.get('imdb_id', 'Unknown')})")
+                    if imdb_id is not None:
+                        movie["imdb_id"] = imdb_id["href"].split("/title/")[1].split("/")[0]
 
-                    # If a movie doesn't have a year, that means that the movie is only just announced and we don't even know when it's coming out. We can easily ignore these because movies will have a year of release by the time they come out. 
+                    if movie_year is not None:
+                        movie["release_year"] = movie_year.text
+                       
+                # If a movie doesn't have a year, that means that the movie is only just announced and we don't even know when it's coming out. We can easily ignore these because movies will have a year of release by the time they come out. 
+                if 'release_year' in movie:
                     movies.append(movie)
 
             if soup.find('a', {'class': 'next'}):
